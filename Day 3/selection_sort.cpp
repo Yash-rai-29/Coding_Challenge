64de68d6de1f0b0bd5faf11b90764353cc79f796
Yash Rai <99464832+Yash-rai-29@users.noreply.github.com>
2023-02-19 18:12:14 +0000

    main

---
+++ Day 3/selection_sort.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int selectionsort(int a[],int n){
+    for(int i=0;i<n-1;i++){
+        int min=i;
+        for(int j=i+1;j<n-1;j++){
+            if(a[j]<a[min]){
+                min=j;
+            }
+             if (min!=i)
+            swap(a[i],a[min]);
+        }
+    }
+}
+int main(){
+    int a[1000],n;
+    cout<<"Enter the number of array elements: ";
+    cin>>n;
+    cout<<"Enter the elements of the array: ";
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    selectionsort(a,n);
+    cout<<"Sorted array: ";
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
