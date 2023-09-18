66c7de574d4aef1291c26dcf0e2ea629a4ae56b0
Yash Rai <99464832+Yash-rai-29@users.noreply.github.com>
2023-09-18 16:57:10 +0000

    M

--- Day 5/string.cpp
+++ Day 5/string.cpp
@@ -1,9 +1,29 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+#include <cstring>  // Added for strlen()
+
 using namespace std;
 
-int main(){
-    string n;
-    
-   
-    
+void removeDuplicates(char a[]) {
+    int l = strlen(a);
+    if (l == 1 || l == 0) {
+        return;
+    }
+    int prev = 0;
+    for (int current = 1; current < l; current++) {
+        if (a[current] != a[prev]) {
+            prev++;
+            a[prev] = a[current];
+        }
+    }
+    a[prev + 1] = '\0';
+}
+
+int main() {
+    char a[1000];
+    cin.getline(a, 1000);
+
+    removeDuplicates(a);
+    cout << a << endl;
+    return 0;
 }
